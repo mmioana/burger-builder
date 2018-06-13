0151b47e8b07eb48b6547a8f3944834e556d7d71
Ioana Mot <mmioana94@gmail.com>
2018-06-13 17:43:04 +0300

    Added dynamic list of ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(key => [...Array(props.ingredients[key])]
+            .map((_, index) => {
+                return (<BurgerIngredient key={key + index} type={key}/>);
+            })
+        );
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='salad'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,14 +7,19 @@ class BurgerBuilder extends Component {
     constructor (props) {
         super(props);
         this.state = {
-
+            ingredients : {
+                salad: 1,
+                cheese: 2,
+                bacon: 1,
+                meat: 2
+            }
         };
     }
 
     render() {
         return (
             <Auxiliary>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
