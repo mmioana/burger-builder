20d705151d51c4a4c4fcfe06fb30876db0b8f364
Ioana Mot <mmioana94@gmail.com>
2018-06-13 17:03:50 +0300

    Added basic burger builder component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import classes from './Layout.css';
 
 const layout = (props) => {
     return (
         <Auxiliary>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <main>{props.children}</main>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
         </Auxiliary>
     );
 };
