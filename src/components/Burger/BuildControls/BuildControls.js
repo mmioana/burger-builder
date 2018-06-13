478a756f92f592e78c82d371f912fcade9eb7ae6
Ioana Mot <mmioana94@gmail.com>
2018-06-13 18:10:44 +0300

    Added build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => {
     return (
         <div className={classes.BuildControls}>
+            {controls.map((control) => (
+              <BuildControl
+                  key={control.label}
+                  label={control.label}
+                  type={control.type}/>
+            ))}
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -20,7 +21,7 @@ class BurgerBuilder extends Component {
         return (
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Auxiliary>
         );
     }
