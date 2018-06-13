bb83b2887c5b7dd11c616f8a129ce42fc875fe36
Ioana Mot <mmioana94@gmail.com>
2018-06-13 17:33:02 +0300

    Created hard-coded burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
     render() {
         return (
             <Auxiliary>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
