9001ceb1d009892323843cf4b2c483eb3728d9de
Ioana Mot <mmioana94@gmail.com>
2018-06-14 12:58:48 +0300

    Added toggle functionality for side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => {
-    return (
-        <Auxiliary>
-            <div>Toolbar, SideDrawer, Backdrop</div>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Auxiliary>
-    );
-};
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState, props) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Toolbar toggle={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.css';
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer];
+
+    if (props.open) {
+        attachedClasses.push(classes.Open);
+    } else {
+        attachedClasses.push(classes.Close);
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Auxiliary>
+            <Backdrop show={props.open} clicked={props.close}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>);
+        </Auxiliary>
+    );
 }
 
 export default sideDrawer;
