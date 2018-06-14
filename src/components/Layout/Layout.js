da7ba7f22d4334677f3a60c3055d0229c91d30c0
Ioana Mot <mmioana94@gmail.com>
2018-06-14 12:18:46 +0300

    Added side drawer for mobile and stylized logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 const layout = (props) => {
     return (
         <Auxiliary>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            <Toolbar></Toolbar>
+            <Toolbar />
+            <SideDrawer />
             <main className={classes.Content}>
                 {props.children}
             </main>
